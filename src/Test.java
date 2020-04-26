6e94d2ab19d424767817830b756be10f31d7068a
sowoon <janus7359@gmail.com>
2020-04-26 22:44:29 +0900

    11729

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
+    static String ans;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(br.readLine());
+        Test test = new Test();
+        ans = Integer.toString((int) (Math.pow(2,n)-1)) + "\n";
+        test.hanoi(n, 1, 2, 3);
+        System.out.println(ans);
+    }
+
+    void hanoi(int n, int start, int via, int end) {
+        if (n == 1) {
+            ans += start + " " + end + "\n";
+        } else {
+            hanoi(n - 1, start, end, via);
+            ans += start + " " + end + "\n";
+            hanoi(n - 1, via, start, end);
+        }
     }
 }
