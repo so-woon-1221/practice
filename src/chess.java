3a916656ef0dd9fb23bbb5a483a17ca49d6a215d
sowoon <janus7359@gmail.com>
2020-04-27 23:34:57 +0900

    브루트 포스 1018번

---
+++ src/chess.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class chess {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String num = sc.nextLine();
+        StringTokenizer token = new StringTokenizer(num);
+        int a = Integer.parseInt(token.nextToken());
+        int b = Integer.parseInt(token.nextToken());
+        String[][] board = new String[a][b];
+        int[][] changes = new int[a][b];
+        String[] bb = new String[]{"B", "W", "B", "W", "B", "W", "B", "W"};
+        String[] ww = new String[]{"W", "B", "W", "B", "W", "B", "W", "B"};
+        for (int i = 0; i < a; i++) {
+            String bw = sc.nextLine();
+            String[] prob = bw.split("");
+            for (int j = 0; j < b; j++) {
+                board[i][j] = prob[j];
+                changes[i][j] = -1;
+            }
+        }
+
+        for (int i = 0; i <= a - 8; i++) {
+            for (int j = 0; j <= b - 8; j++) {
+                int change = 0;
+                int count = 0;
+                if (board[i][j].equals("B")) {
+                    System.out.println("B로 시작");
+                    int div = i % 2;
+                    for (int m = i; m < i + 8; m++) {
+                        count = 0;
+                        for (int n = j; n < j + 8; n++) {
+                            if (m % 2 == div) {
+                                if (!bb[count].equals(board[m][n])) {
+                                    System.out.println(m + "째 줄" + n + "번 줄 바꿔야함");
+                                    change++;
+                                }
+                            } else {
+                                if (!ww[count].equals(board[m][n])) {
+                                    System.out.println(m + "째 줄" + n + "번 줄 바꿔야함");
+                                    change++;
+                                }
+                            }
+                            count++;
+                        }
+                    }
+                } else {
+                    System.out.println("W로 시작");
+                    int div = i % 2;
+                    System.out.println(div);
+                    for (int m = i; m < i + 8; m++) {
+                        count = 0;
+                        for (int n = j; n < j + 8; n++) {
+                            if (m % 2 == div) {
+                                if (!ww[count].equals(board[m][n])) {
+                                    System.out.println(m + "째 줄" + n + "번 줄 바꿔야함");
+                                    change++;
+                                }
+                            } else {
+                                if (!bb[count].equals(board[m][n])) {
+                                    System.out.println(m + "째 줄" + n + "번 줄 바꿔야함");
+                                    change++;
+                                }
+                            }
+                            count++;
+                        }
+                    }
+                }
+                System.out.println(i + "째줄" + j + "번쨰 칸은" + change + "번 바꿈");
+            }
+        }
+        int min = 70;
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                if (changes[i][j] != -1) {
+                    if (min > changes[i][j]) {
+                        min = changes[i][j];
+                    }
+                }
+            }
+        }
+        System.out.println(min);
+    }
+}
